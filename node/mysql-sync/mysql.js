0616ff61f06f8b8f90397682594540f937bf200f
JunHyeon Mun <impelfin@gmail.com>
2022-07-13 17:43:03 +0900

    sync-mysql

---
+++ node/mysql-sync/mysql.js
@@ -0,0 +1,51 @@
+var mysql = require("sync-mysql");
+
+var connection = new mysql({
+		host : "database-1.c9tceinqozeu.ap-northeast-2.rds.amazonaws.com",
+		user : "admin",
+		password : "admin1234",
+		database : "st_db",
+});
+
+// Select all rows from st_info table
+let result = connection.query("SELECT * FROM st_info");
+console.log(result);
+
+// insert data
+let data = {
+    ST_ID : 202004,
+    NAME : "Moon",
+    DEPT : "Computer"
+};
+
+// inserted data's id
+let insertId = data.ST_ID;
+
+// insert data into st_info table
+result = connection.query(
+    "INSERT INTO st_info values (?, ?, ?)", [
+        insertId, data.NAME, data.DEPT
+]);
+
+// Select all rows from st_info table
+result = connection.query("SELECT * FROM st_info");
+console.log(result);
+
+// update row
+result = connection.query("UPDATE st_info SET DEPT=? WHERE ST_ID=?", [
+    "Game",
+    insertId,
+]);
+
+// Select all rows from st_info table
+result = connection.query("SELECT * FROM st_info");
+console.log(result);
+
+// delete row
+result = connection.query("DELETE FROM st_info WHERE ST_ID=?", [
+    insertId,
+]);
+
+// Select all rows from st_info table
+result = connection.query("SELECT * FROM st_info");
+console.log(result);
